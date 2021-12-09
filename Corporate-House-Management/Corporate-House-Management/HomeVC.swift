b5f4286591e2366675479dbe5edc75717780c3f8
Mr. Kesh <kes.cemil@gmail.com>
2021-12-09 13:34:49 -0800

    Screen controller initialized

---
+++ Corporate-House-Management/Corporate-House-Management/HomeVC.swift
@@ -0,0 +1,29 @@
+//
+//  HomeVC.swift
+//  Corporate-House-Management
+//
+//  Created by Mr Kes on 12/9/21.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
