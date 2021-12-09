67bb06aa120e84a6e0b5b0b16b7c2c00509f90af
Mr. Kesh <kes.cemil@gmail.com>
2021-12-09 09:09:02 -0800

    Project added

---
+++ Corporate-House-Management/Corporate-House-ManagementTests/Corporate_House_ManagementTests.swift
@@ -0,0 +1,33 @@
+//
+//  Corporate_House_ManagementTests.swift
+//  Corporate-House-ManagementTests
+//
+//  Created by Mr Kes on 12/9/21.
+//
+
+import XCTest
+@testable import Corporate_House_Management
+
+class Corporate_House_ManagementTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
---
+++ Corporate-House-Management/Corporate-House-ManagementUITests/Corporate_House_ManagementUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Corporate_House_ManagementUITestsLaunchTests.swift
+//  Corporate-House-ManagementUITests
+//
+//  Created by Mr Kes on 12/9/21.
+//
+
+import XCTest
+
+class Corporate_House_ManagementUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
