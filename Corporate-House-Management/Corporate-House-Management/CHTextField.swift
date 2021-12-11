4055f3049f0835a603a7a6db7b950a218d2d9ae1
Mr. Kesh <kes.cemil@gmail.com>
2021-12-11 08:54:43 -0800

    CHTextfield (custom textfield) configurations added

---
+++ Corporate-House-Management/Corporate-House-Management/CHTextField.swift
@@ -0,0 +1,45 @@
+//
+//  CHTextField.swift
+//  Corporate-House-Management
+//
+//  Created by Mr Kes on 12/11/21.
+//
+
+import UIKit
+
+class CHTextField: UITextField {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        layer.cornerRadius          = 10
+        layer.borderWidth           = 2
+        layer.borderColor           = UIColor.systemGray4.cgColor
+        
+        textColor                   = .label
+        tintColor                   = .label //blinking cursor
+        textAlignment               = .center
+        font                        = UIFont.preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth   = true
+        minimumFontSize             = 12
+        
+        backgroundColor             = .tertiarySystemBackground
+        autocorrectionType          = .no
+        placeholder                 = "Enter a username"
+        
+    }
+    
+    
+    
+}
