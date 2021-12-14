852570c1f43a69971f3be859afff989d8d04423a
Mr. Kesh <kes.cemil@gmail.com>
2021-12-14 14:03:47 -0800

    UIView shadow extension added

---
+++ Corporate-House-Management/Corporate-House-Management/UIView+Extension.swift
@@ -0,0 +1,22 @@
+//
+//  UIView+Extension.swift
+//  Corporate-House-Management
+//
+//  Created by Mr Kes on 12/14/21.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func applyShadow(cornerRadius: CGFloat) {
+        layer.cornerRadius      = cornerRadius
+        layer.masksToBounds     = true
+        layer.shadowRadius      = 4.0
+        layer.shadowOpacity     = 0.30
+        layer.shadowColor       = UIColor.gray.cgColor
+        layer.shadowOffset      = CGSize(width: 0, height: 5)
+    }
+    
+    
+}
