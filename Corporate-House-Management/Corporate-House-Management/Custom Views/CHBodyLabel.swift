e9e7ac7ad5dc407aae51a7488a0c80e073b33a59
Mr. Kesh <kes.cemil@gmail.com>
2021-12-18 16:17:27 -0800

    Custom Body Label class added

---
+++ Corporate-House-Management/Corporate-House-Management/Custom Views/CHBodyLabel.swift
@@ -0,0 +1,38 @@
+//
+//  CHBodyLabel.swift
+//  Corporate-House-Management
+//
+//  Created by Mr Kes on 12/18/21.
+//
+
+import UIKit
+
+class CHBodyLabel: UILabel {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    init(textAlignment: NSTextAlignment, fontSize: CGFloat) {
+        super.init(frame: .zero)
+        self.textAlignment = textAlignment
+        configure()
+    }
+    
+    
+    private func configure() {
+        textColor                   = .secondaryLabel
+        font                        = UIFont.preferredFont(forTextStyle: .body)
+        adjustsFontSizeToFitWidth   = true
+        minimumScaleFactor          = 0.75
+        lineBreakMode               = .byWordWrapping
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
