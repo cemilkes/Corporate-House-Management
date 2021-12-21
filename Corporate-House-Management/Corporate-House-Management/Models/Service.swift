b3f7fb80a64e1a3bdb375e03a0046ce5de85e2db
Mr. Kesh <kes.cemil@gmail.com>
2021-12-20 16:20:53 -0800

    Service model added

---
+++ Corporate-House-Management/Corporate-House-Management/Models/Service.swift
@@ -0,0 +1,21 @@
+//
+//  Service.swift
+//  Corporate-House-Management
+//
+//  Created by Mr Kes on 12/20/21.
+//
+
+import Foundation
+
+struct Service {
+    let id: String
+    let service: String
+    let fee: Double
+    let additionalNote: String?
+    let receiptImageLink: String?
+    let serviceDate: String
+    let createdAt: String
+    let lastEditAt: Date
+    let editedBy: Date
+    
+}
